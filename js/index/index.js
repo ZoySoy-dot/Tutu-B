d5500b97d88221a81ad65c2a851671cc9576d819
Renz Aron Gorre <1ragorre1@gmail.com>
2025-03-18 11:53:23 +0800

    Implement smooth scrolling for Vision Statement section; replace arrow link with button and update styles

---
+++ js/index/index.js
@@ -0,0 +1,3 @@
+document.getElementById("header-navigation-button").addEventListener("click", function() {
+    document.getElementById("Vision_Statement").scrollIntoView({behavior: "smooth"});
+});
