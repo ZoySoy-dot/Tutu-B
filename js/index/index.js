7b394bbe5912772eafaa76fe627eeed5ee8f8f8c
Renz Aron Q. Gorre <1ragorre1@gmail.com>
2025-03-23 22:00:42 +0800

    Implement hamburger menu functionality and add main.js script for navigation

--- js/index/index.js
+++ js/index/index.js
@@ -55,20 +55,3 @@ informationButton.forEach((element) => {
   });
 });
 
-const hamburger = document.getElementsByClassName("Hamburger-Menu-Button")[0];
-
-hamburger.addEventListener("click", function () {
-  const navigation = document.getElementsByClassName("Hamburger-Menu")[0];
-
-  navigation.style.display = "flex";
-  document.querySelector("html").style.overflow = "hidden";
-});
-
-const hamburgerClose = document.getElementsByClassName("Hamburger-Menu_Button-Close")[0];
-
-hamburgerClose.addEventListener("click", function () {
-    const navigation = document.getElementsByClassName("Hamburger-Menu")[0];
-    
-    navigation.style.display = "none";
-    document.querySelector("html").style.overflow = "auto";
-});
